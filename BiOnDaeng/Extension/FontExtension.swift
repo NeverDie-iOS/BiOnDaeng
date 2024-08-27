f0a8d69c5bc44aa73167039e748a777a65e5c742
hyunsoo <mm083434@gmail.com>
2024-08-27 19:43:26 +0900

    🟡 : Font 사용을 위한 FontExtension 파일 생성

---
+++ BiOnDaeng/Extension/FontExtension.swift
@@ -0,0 +1,6 @@
+import SwiftUI
+
+extension Font {
+    static let pretendardSemiBold13: Font = .custom("Pretendard-SemiBold", size: 13)
+    static let pretendardSemiBold18: Font = .custom("Pretendard-SemiBold", size: 18)
+}
