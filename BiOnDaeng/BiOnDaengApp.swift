118df65a2206ab2de908f7a050d578450dce7ce5
hyunsoo <mm083434@gmail.com>
2024-08-27 17:51:33 +0900

    Initial Commit

---
+++ BiOnDaeng/BiOnDaengApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiOnDaengApp.swift
+//  BiOnDaeng
+//
+//  Created by HyunSoo on 8/27/24.
+//
+
+import SwiftUI
+
+@main
+struct BiOnDaengApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
