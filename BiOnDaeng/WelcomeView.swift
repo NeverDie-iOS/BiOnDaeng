2f3c2be47ccfd906265418cc7974676eee108db2
hyunsoo <mm083434@gmail.com>
2024-08-27 18:31:40 +0900

    🟡 : Welcome View 추가

---
+++ BiOnDaeng/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  BiOnDaeng
+//
+//  Created by HyunSoo on 8/27/24.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
