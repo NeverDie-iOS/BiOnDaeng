b2761b1f3bfd81ab411ee2ec699bcf07200210a2
hyunsoo <mm083434@gmail.com>
2024-09-04 01:22:43 +0900

    ⚡️ : MainView 날씨 정보 추가

--- BiOnDaeng/MainView.swift
+++ BiOnDaeng/MainView.swift
@@ -60,6 +60,26 @@ struct MainThemeView: View {
         Image("MainTheme")
             .resizable()
             .padding(.horizontal)
+            .overlay(
+                VStack {
+                    ZStack {
+                        Text("15°")
+                            .font(.pretendardMedium(size: 36))
+                            .foregroundStyle(Color(hex: "FBFCFE")!)
+                        Text("25° / 12°/흐림")
+                            .font(.pretendardExtraLight(size: 7))
+                            .foregroundStyle(Color(hex: "FBFCFE")!)
+                            .offset(x: 42, y: 11)
+                    }
+                    Text("강수 확률 60%")
+                        .font(.pretendardMedium(size: 12))
+                        .foregroundStyle(Color(hex: "FBFCFE")!)
+                        .padding(.leading, 40)
+                }
+                .padding(.top, 20)
+                .padding(.leading, 10),
+                alignment: .topLeading
+                )
     }
 }
 
