acfed5c7c46d469e56ae948bfeb1f3c4cac9d836
hyunsoo <mm083434@gmail.com>
2024-09-22 11:26:09 +0900

    ⚡️ : SettingView 화면 전환 & UI 구현

--- BiOnDaeng/View/MainView.swift
+++ BiOnDaeng/View/MainView.swift
@@ -6,95 +6,95 @@ struct MainView: View {
     @AppStorage("myLocation") var myLocation: String = ""
     
     var body: some View {
-        VStack {
-            Spacer().frame(height: 26)
-            
-            HStack(spacing: 0) {
-                Spacer().frame(width: 21)
+        NavigationView {
+            VStack {
+                Spacer().frame(height: 26)
                 
-                HStack {
-                    Image("Search")
-                        .resizable()
-                        .frame(width: 19, height: 19)
-                        .padding(.vertical, 6)
+                HStack(spacing: 0) {
+                    Spacer().frame(width: 21)
                     
-                    NavigationStack {
-                        Button(action: {
-                            showSheet = true
-                        }) {
-                            if myLocation.isEmpty {
-                                Text("지역(구/동)을 설정하세요.")
-                                    .foregroundStyle(Color.black)
-                                    .font(.pretendardMedium(size: 10))
-                            } else {
-                                Text("\(myLocation)")
-                                    .foregroundStyle(Color.black)
-                                    .font(.pretendardMedium(size: 10))
+                    HStack {
+                        Image("Search")
+                            .resizable()
+                            .frame(width: 19, height: 19)
+                            .padding(.vertical, 6)
+                        
+                        NavigationStack {
+                            Button(action: {
+                                showSheet = true
+                            }) {
+                                if myLocation.isEmpty {
+                                    Text("지역(구/동)을 설정하세요.")
+                                        .foregroundStyle(Color.black)
+                                        .font(.pretendardMedium(size: 10))
+                                } else {
+                                    Text("\(myLocation)")
+                                        .foregroundStyle(Color.black)
+                                        .font(.pretendardMedium(size: 10))
+                                }
+                            }
+                            .fullScreenCover(isPresented: $showSheet) {
+                                LocationSearchView(myLocation: $myLocation)
                             }
                         }
-                        .fullScreenCover(isPresented: $showSheet) {
-                            LocationSearchView(myLocation: $myLocation)
-                        }
                     }
-                }
-                .frame(width: 216, height: 31)
-                .background(Color.white)
-                .cornerRadius(8)
-                .overlay(RoundedRectangle(cornerRadius: 8)
-                    .stroke(Color(hex: "E0E0E0")!, lineWidth: 1)
-                )
-                .shadow(color: Color.black.opacity(0.1), radius: 4, x: 0, y: 2)
-                
-                Button(action: {
-                    locationManager.requestCurrentLocation()
-                }) {
-                    Image("CurrentLocation")
-                        .resizable()
-                        .frame(width: 27, height: 27)
-                        .scaledToFit()
-                }.alert(isPresented: $locationManager.showAlert) {
-                    Alert(
-                        title: Text("위치 권한 필요"),
-                        message: Text("위치 권한이 거부되었습니다.\n설정에서 권한을 변경해 주세요!"),
-                        primaryButton: .default(Text("설정으로 이동")) {
-                            locationManager.openSettings()
-                        },
-                        secondaryButton: .cancel(Text("취소"))
+                    .frame(width: 216, height: 31)
+                    .background(Color.white)
+                    .cornerRadius(8)
+                    .overlay(RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color(hex: "E0E0E0")!, lineWidth: 1)
                     )
+                    .shadow(color: Color.black.opacity(0.1), radius: 4, x: 0, y: 2)
+                    
+                    Button(action: {
+                        locationManager.requestCurrentLocation()
+                    }) {
+                        Image("CurrentLocation")
+                            .resizable()
+                            .frame(width: 27, height: 27)
+                            .scaledToFit()
+                    }.alert(isPresented: $locationManager.showAlert) {
+                        Alert(
+                            title: Text("위치 권한 필요"),
+                            message: Text("위치 권한이 거부되었습니다.\n설정에서 권한을 변경해 주세요!"),
+                            primaryButton: .default(Text("설정으로 이동")) {
+                                locationManager.openSettings()
+                            },
+                            secondaryButton: .cancel(Text("취소"))
+                        )
+                    }
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        
+                    }) {
+                        Image("Share")
+                            .resizable()
+                            .foregroundColor(Color.black)
+                            .frame(width: 23, height: 23)
+                    }
+                    
+                    Spacer().frame(width: 6)
+                    
+                    NavigationLink(destination: SettingView()) {
+                                        Image("Setting")
+                                            .resizable()
+                                            .frame(width: 20, height: 20)
+                                            .scaledToFit()
+                                    }
+                    
+                    Spacer().frame(width: 20)
                 }
+                
+                TabView {
+                    MainThemeView()
+                    DetailMainView()
+                }
+                .tabViewStyle(PageTabViewStyle())
                 
                 Spacer()
-                
-                Button(action: {
-                    
-                }) {
-                    Image("Share")
-                        .resizable()
-                        .foregroundColor(Color.black)
-                        .frame(width: 23, height: 23)
-                }
-                
-                Spacer().frame(width: 6)
-                
-                Button(action: {
-                    
-                }) {
-                    Image("Setting")
-                        .resizable()
-                        .frame(width: 20, height: 20)
-                        .scaledToFit()
-                }
-                
-                Spacer().frame(width: 20)
             }
-            
-            TabView {
-                MainThemeView()
-                DetailMainView()
-            }
-            .tabViewStyle(PageTabViewStyle())
-            
-            Spacer()
         }
     }
 }
--- BiOnDaeng/View/SettingView.swift
+++ BiOnDaeng/View/SettingView.swift
@@ -1,11 +1,84 @@
 import SwiftUI
 
 struct SettingView: View {
+    @Environment(\.presentationMode) var presentationMode
+
     var body: some View {
-        EmptyView()
+        VStack {
+            HStack {
+                Text("알림 설정")
+                    .font(.pretendardMedium(size: 14))
+                    .padding(.leading, 16)
+                
+                Spacer()
+                
+                Button("오전 08:00") {
+                    
+                }
+                .font(.pretendardMedium(size: 14))
+                .foregroundStyle(Color.black)
+                .padding(.trailing, 12)
+            }
+            .frame(width: 333 * UIScreen.main.bounds.width / 393, height: 67 * UIScreen.main.bounds.height / 852)
+            .background(Color(hex: "FFFCF1"))
+            .overlay( RoundedRectangle(cornerRadius: 16)
+                .stroke(Color(hex: "FFE58C")!, lineWidth: 2)
+            )
+            .padding(.top, 27)
+        
+            
+            HStack {
+                Text("비 알림")
+                    .font(.pretendardMedium(size: 14))
+                    .padding(.leading, 16)
+                Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                    
+                }
+                .padding(.trailing, 12)
+            }
+            .frame(width: 333 * UIScreen.main.bounds.width / 393, height: 67 * UIScreen.main.bounds.height / 852)
+            .background(Color(hex: "FFFCF1"))
+            .overlay( RoundedRectangle(cornerRadius: 16)
+                .stroke(Color(hex: "FFE58C")!, lineWidth: 2)
+            )
+            .padding(.top, 13)
+            
+            HStack {
+                Text("지역 설정")
+                    .font(.pretendardMedium(size: 14))
+                    .padding(.leading, 16)
+                
+                Spacer()
+                
+                Text("광주광역시 북구")
+                    .font(.pretendardMedium(size: 14))
+                    .padding(.trailing, 12)
+            }
+            .frame(width: 333 * UIScreen.main.bounds.width / 393, height: 67 * UIScreen.main.bounds.height / 852)
+            .background(Color(hex: "FFFCF1"))
+            .overlay( RoundedRectangle(cornerRadius: 16)
+                .stroke(Color(hex: "FFE58C")!, lineWidth: 2)
+            )
+            .padding(.top, 13)
+            
+            Spacer()
+            
+        }
+        .navigationTitle("설정")
+        .navigationBarBackButtonHidden(true)
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationBarItems(leading: Button(action: {
+            presentationMode.wrappedValue.dismiss()
+        }) {
+            Image("Back")
+                .resizable()
+                .frame(width: 26, height: 25)
+        })
     }
 }
 
-#Preview {
-    SettingView()
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
 }
