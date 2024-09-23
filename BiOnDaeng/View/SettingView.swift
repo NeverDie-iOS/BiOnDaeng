6efa884089ba2178d80eea51d1ba2827d388fd79
hyunsoo <mm083434@gmail.com>
2024-09-24 06:53:25 +0900

    ⚡️: 알람 시간 설정 기능 추가

--- BiOnDaeng/View/SettingView.swift
+++ BiOnDaeng/View/SettingView.swift
@@ -2,7 +2,10 @@ import SwiftUI
 
 struct SettingView: View {
     @Environment(\.presentationMode) var presentationMode
-
+    @AppStorage("selectedTime") var selectedTime: String = "08:00"
+    @State var showSheet = false
+    @State private var tempSelectedTime: Date = Date()
+    
     var body: some View {
         VStack {
             HStack {
@@ -12,33 +15,94 @@ struct SettingView: View {
                 
                 Spacer()
                 
-                Button("오전 08:00") {
-                    
+                Button("\(convertTimeToAMPM(selectedTime: selectedTime))") {
+                    showSheet = true
+                    let components = selectedTime.split(separator: ":").map { Int($0) ?? 0 }
+                    tempSelectedTime = Calendar.current.date(from: DateComponents(hour: components[0], minute: components[1])) ?? Date()
                 }
                 .font(.pretendardMedium(size: 14))
                 .foregroundStyle(Color.black)
                 .padding(.trailing, 12)
+                .sheet(isPresented: $showSheet) {
+                    VStack {
+                        Image(systemName: "alarm.fill")
+                            .resizable()
+                            .frame(width: 27.0, height: 27.0)
+                            .foregroundColor(Color.black)
+                            .padding(.top, 12)
+                        
+                        Text("알람 설정")
+                            .foregroundStyle(Color.black)
+                            .font(.pretendardSemiBold(size: 20))
+                            .padding(.top, 7)
+                        
+                        Text("비비가 시간에 맞춰 알려드릴게요!")
+                            .foregroundStyle(Color(hex: "A5A5A5")!)
+                            .font(.pretendardSemiBold(size: 13))
+                            .padding(.top, 15)
+                        
+                        DatePicker("알람 시간", selection: $tempSelectedTime, displayedComponents: .hourAndMinute)
+                            .datePickerStyle(.wheel)
+                            .frame(width: 150, height: 150)
+                            .padding(.top, 20)
+                        
+                        Spacer()
+                        
+                        HStack(spacing: 21) {
+                            Button(action: {
+                                showSheet = false
+                            }) {
+                                Text("취소")
+                                    .foregroundStyle(.black)
+                            }
+                            .frame(width: 160, height: 50)
+                            .background(Color.white)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color(.black), lineWidth: 0.5)
+                            )
+                            
+                            Button(action: {
+                                showSheet = false
+                                let formatter = DateFormatter()
+                                formatter.dateFormat = "HH:mm"
+                                selectedTime = formatter.string(from: tempSelectedTime)
+                            }) {
+                                Text("선택완료")
+                                    .foregroundStyle(.white)
+                            }
+                            .frame(width: 160, height: 50)
+                            .background(Color(hex: "4B81C2"))
+                            .cornerRadius(8)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color(.white), lineWidth: 0.5)
+                            )
+                        }
+                        Spacer()
+                    }
+                    .presentationDetents([.height(400)])
+                }
             }
             .frame(width: 333 * UIScreen.main.bounds.width / 393, height: 67 * UIScreen.main.bounds.height / 852)
             .background(Color(hex: "FFFCF1"))
-            .overlay( RoundedRectangle(cornerRadius: 16)
+            .overlay(RoundedRectangle(cornerRadius: 16)
                 .stroke(Color(hex: "FFE58C")!, lineWidth: 2)
             )
             .padding(.top, 27)
         
-            
             HStack {
                 Text("비 알림")
                     .font(.pretendardMedium(size: 14))
                     .padding(.leading, 16)
-                Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                Toggle(isOn: .constant(true)) {
                     
                 }
                 .padding(.trailing, 12)
             }
             .frame(width: 333 * UIScreen.main.bounds.width / 393, height: 67 * UIScreen.main.bounds.height / 852)
             .background(Color(hex: "FFFCF1"))
-            .overlay( RoundedRectangle(cornerRadius: 16)
+            .overlay(RoundedRectangle(cornerRadius: 16)
                 .stroke(Color(hex: "FFE58C")!, lineWidth: 2)
             )
             .padding(.top, 13)
@@ -56,7 +120,7 @@ struct SettingView: View {
             }
             .frame(width: 333 * UIScreen.main.bounds.width / 393, height: 67 * UIScreen.main.bounds.height / 852)
             .background(Color(hex: "FFFCF1"))
-            .overlay( RoundedRectangle(cornerRadius: 16)
+            .overlay(RoundedRectangle(cornerRadius: 16)
                 .stroke(Color(hex: "FFE58C")!, lineWidth: 2)
             )
             .padding(.top, 13)
@@ -74,6 +138,17 @@ struct SettingView: View {
                 .resizable()
                 .frame(width: 26, height: 25)
         })
+    }
+    
+    func convertTimeToAMPM(selectedTime: String) -> String {
+        let components = selectedTime.split(separator: ":").map { Int($0) }
+        guard let hour = components[0], let minute = components[1] else {
+            return selectedTime
+        }
+        
+        let period = hour < 12 ? "오전" : "오후"
+        let adjustedHour = hour % 12 == 0 ? 12 : hour % 12 // 12시 조정
+        return String(format: "%@ %02d:%02d", period, adjustedHour, minute)
     }
 }
 
