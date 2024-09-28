2d811c1cd6a01fc491195e0f309a36f3db72c34c
hyunsoo <mm083434@gmail.com>
2024-09-29 08:47:34 +0900

    💩 : 로컬 알림 부분 코드 삭제

--- BiOnDaeng/BiOnDaengApp.swift
+++ BiOnDaeng/BiOnDaengApp.swift
@@ -22,15 +22,6 @@ class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDele
     @AppStorage("notificationPermission") var notificationPermission: Bool = false
     @AppStorage("bionNotifi") var bionNotifi: Bool = false
     
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-        FirebaseApp.configure()
-        requestNotificationAuthorization()
-        Messaging.messaging().delegate = self
-        scheduleLocalNotification(at: 4, minute: 1, title: "알람 테스트", body: "테스트입니다요.")
-        return true
-    }
-    
     func requestNotificationAuthorization() {
         let center = UNUserNotificationCenter.current()
         center.requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
@@ -46,40 +37,11 @@ class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDele
         UIApplication.shared.registerForRemoteNotifications()
     }
 
-    func scheduleLocalNotification(at hour: Int, minute: Int, title: String, body: String) {
-        let content = UNMutableNotificationContent()
-        content.title = title
-        content.body = body
-        content.sound = UNNotificationSound.default
-
-        var dateComponents = DateComponents()
-        dateComponents.hour = hour
-        dateComponents.minute = minute
-
-        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
-        
-        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-
-        UNUserNotificationCenter.current().add(request) { error in
-            if let error = error {
-                print("로컬 알림 예약 실패: \(error)")
-            }
-        }
-    }
-
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         Messaging.messaging().apnsToken = deviceToken
     }
 
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
         print("등록 토큰 수신: \(String(describing: fcmToken))")
-    }
-
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-        completionHandler()
-    }
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([.banner, .sound])
     }
 }
