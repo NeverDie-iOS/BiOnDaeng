e0408b002ca5d3b59014599d1372d9f05cb45bd6
hyunsoo <mm083434@gmail.com>
2024-09-29 02:52:38 +0900

    ⚡️ : CSV 파일에서 위도/경도도 가져오도록 설정 --> CCTV에 이용

--- BiOnDaeng/ViewModel/SearchBarViewModel.swift
+++ BiOnDaeng/ViewModel/SearchBarViewModel.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-func loadCSV() -> [String: (x: String, y: String)] {
+func loadCSV() -> [String: (x: String, y: String, longitude: String, latitude: String)] {
     guard let path = Bundle.main.path(forResource: "동네예보지점좌표240101", ofType: "csv") else {
         print("파일 경로를 찾을 수 없습니다.")
         return [:]
@@ -10,27 +10,28 @@ func loadCSV() -> [String: (x: String, y: String)] {
         let data = try String(contentsOfFile: path)
         let rows = data.components(separatedBy: "\n").dropFirst()
         
-        var locations: [String: (x: String, y: String)] = [:]
+        var locations: [String: (x: String, y: String, longitude: String, latitude: String)] = [:]
         
         for row in rows {
             let columns = row.components(separatedBy: ",")
-            if columns.count > 5 { 
-                let region = columns[2]
-                let district = columns[3]
-                let neighborhood = columns[4]
-                let xCoordinate = columns[5]
-                let yCoordinate = columns[6]
-                
-                let locationName = "\(region) \(district) \(neighborhood)"
-                    .trimmingCharacters(in: .whitespaces)
-                
-                locations[locationName] = (x: xCoordinate, y: yCoordinate)
-            }
-        }
-        
-        return locations
-    } catch {
-        print("CSV 파일을 읽는 중 오류 발생: \(error)")
-        return [:]
+            
+            let region = columns[2]
+            let district = columns[3]
+            let neighborhood = columns[4]
+            let xCoordinate = columns[5]
+            let yCoordinate = columns[6]
+            let longitude = columns[13] // 경도
+            let latitude = columns[14] // 위도
+            
+            let locationName = "\(region) \(district) \(neighborhood)"
+                .trimmingCharacters(in: .whitespaces)
+            
+            locations[locationName] = (x: xCoordinate, y: yCoordinate, longitude: longitude, latitude: latitude)
+    }
+    
+    return locations
+} catch {
+    print("CSV 파일을 읽는 중 오류 발생: \(error)")
+    return [:]
     }
 }
