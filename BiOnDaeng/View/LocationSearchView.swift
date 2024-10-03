1d8a20f16ae4243f3f16fbc4240c3aa020f16a48
hyunsoo <mm083434@gmail.com>
2024-10-03 13:16:37 +0900

    ⚡️ : DB테이블 nx / ny 값 put 구현

--- BiOnDaeng/View/LocationSearchView.swift
+++ BiOnDaeng/View/LocationSearchView.swift
@@ -1,21 +1,29 @@
 import SwiftUI
+import Alamofire
 
 struct LocationSearchView: View {
     @State var searchText: String = ""
-    @State private var locations: [String: (String, String,String,String)] = loadCSV()
+    @State private var locations: [String: (String, String, String, String)] = loadCSV()
     @Binding var myLocation: String
     @Environment(\.presentationMode) var presentationMode
     @AppStorage("nx") var nx: String = "0"
     @AppStorage("ny") var ny: String = "0"
     @AppStorage("longitude") var longitude: String = "0"
     @AppStorage("latitude") var latitude: String = "0"
+    @StateObject private var networkMonitor = NetworkMonitor()
+    @AppStorage("uuid") private var uuid: String = ""
+    @AppStorage("fcmToken") private var fcmToken: String = ""
+    @AppStorage("baseTime") private var baseTime: String = ""
+    @AppStorage("isYesterday") private var isYesterday: Bool = false
+    @AppStorage("alarmPermission") private var alarmPermission: Bool = false
+    @State private var showAlert = false // 네트워크 연결 불안정
+    @AppStorage("id") private var id: Int = 0
     
     var filteredLocations: [String] {
-        if searchText.isEmpty || searchText == " "{
+        if searchText.isEmpty || searchText == " " {
             return []
         } else {
-            return locations.keys.filter { $0.contains(searchText) }.sorted { $0.count < $1.count
-            }
+            return locations.keys.filter { $0.contains(searchText) }.sorted { $0.count < $1.count }
         }
     }
     
@@ -50,19 +58,143 @@ struct LocationSearchView: View {
             
             List(filteredLocations, id: \.self) { location in
                 Button(action: {
-                    myLocation = location
-                    nx = locations[location]!.0
-                    ny = locations[location]!.1
-                    longitude = locations[location]!.2
-                    latitude = locations[location]!.3
-                    presentationMode.wrappedValue.dismiss()
+                    if networkMonitor.isConnected {
+                        fetchRecordID(using: uuid)
+                        myLocation = location
+                        nx = locations[location]!.0
+                        ny = locations[location]!.1
+                        longitude = locations[location]!.2
+                        latitude = locations[location]!.3
+                        
+                        alarmPermission = checkNotificationPermissionSync()
+                        saveLocationToDatabase()
+                        
+                        presentationMode.wrappedValue.dismiss()
+                    } else {
+                        showAlert = true
+                    }
                 }) {
                     Text("\(location)")
                 }
-                        }
+            }
             .listStyle(.inset)
             .padding(0)
         }
+        .alert("네트워크 연결이 불안정합니다. 다시 시도해주세요", isPresented: $showAlert) {
+            Button("확인", role: .cancel) {}
+        }
         .padding(0)
     }
+    
+    func saveLocationToDatabase() {
+        checkUUIDExists { exists in
+            if exists {
+                updateLocationInDatabase()
+            } else {
+                addLocationToDatabase()
+            }
+        }
+    }
+
+    func checkUUIDExists(completion: @escaping (Bool) -> Void) {
+        let url = "http://211.188.54.174:1337/alarms?uuid=\(uuid)"
+
+        AF.request(url)
+            .validate()
+            .response { response in
+                if response.response?.statusCode == 200 {
+                    print("uuid 존재")
+                    completion(true)
+                } else {
+                    print("uuid 존재안함")
+                    completion(false)
+                }
+            }
+    }
+
+    func updateLocationInDatabase() {
+        let url = "http://211.188.54.174:1337/alarms/\(id)"
+        let parameters: [String: Any] = [
+            "nx": nx,
+            "ny": ny
+        ]
+        
+        AF.request(url, method: .put, parameters: parameters, encoding: JSONEncoding.default)
+            .validate()
+            .response { response in
+                switch response.result {
+                case .success:
+                    print("위치 정보가 성공적으로 업데이트되었습니다.")
+                case .failure(let error):
+                    print("위치 정보 업데이트 실패: \(error)")
+                    if let data = response.data, let errorMessage = String(data: data, encoding: .utf8) {
+                        print("서버 응답: \(errorMessage)")
+                    }
+                }
+            }
+    }
+
+    func addLocationToDatabase() {
+        let url = "http://211.188.54.174:1337/alarms" // 추가 URL
+        let parameters: [String: Any] = [
+            "uuid": uuid,
+            "nx": nx,
+            "ny": ny,
+            "base_time": baseTime,
+            "isYesterday": isYesterday,
+            "alarmPermission": alarmPermission,
+            "fcmToken": fcmToken
+        ]
+        
+        AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default)
+            .validate()
+            .response { response in
+                switch response.result {
+                case .success:
+                    print("위치 정보가 성공적으로 저장되었습니다.")
+                case .failure(let error):
+                    print("위치 정보 저장 실패: \(error)")
+                    if let data = response.data, let errorMessage = String(data: data, encoding: .utf8) {
+                        print("서버 응답: \(errorMessage)")
+                    }
+                }
+            }
+    }
+    
+    func checkNotificationPermissionSync() -> Bool {
+        let semaphore = DispatchSemaphore(value: 0)
+        var isAuthorized = false
+
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            isAuthorized = settings.authorizationStatus == .authorized
+            semaphore.signal()
+        }
+
+        semaphore.wait()
+        return isAuthorized
+    }
+    
+    func fetchRecordID(using uuid: String) {
+        let url = "http://211.188.54.174:1337/alarms?uuid=\(uuid)"
+        
+        AF.request(url)
+               .validate()
+               .responseDecodable(of: [Record].self) { response in
+                   switch response.result {
+                   case .success(let records):
+                       if let firstRecord = records.first {
+                           id = firstRecord.id
+                           print(firstRecord.id)
+                       } else {
+                       }
+                   case .failure(let error):
+                       print("Error: \(error)")
+                   }
+               }
+    }
+}
+
+struct Record: Codable {
+    let id: Int
+    let uuid: String
 }
