5a19a513dd974441fce57ac62f7088956046c53d
hyunsoo <mm083434@gmail.com>
2024-10-04 15:30:52 +0900

    ⚡️: 현재 위치 버튼 클릭 시 DB에 nx/ny값 업데이트 🔧 : 현재 위치 버튼 클릭 시 다른 동네 ny값 가져오는 버그 fix (오타였음)

--- BiOnDaeng/ViewModel/MainViewModel.swift
+++ BiOnDaeng/ViewModel/MainViewModel.swift
@@ -13,6 +13,10 @@ class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
     @AppStorage("ny") var ny: String = "0"
     @AppStorage("longitude") var longitude: String = "0"
     @AppStorage("latitude") var latitude: String = "0"
+    @StateObject private var networkMonitor = NetworkMonitor()
+    @State private var showNetworkAlert = false // 네트워크 연결 불안정
+    @AppStorage("id") private var id: Int = 0
+    @AppStorage("uuid") private var uuid: String = ""
     
     override init() {
         super.init()
@@ -58,9 +62,11 @@ class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
                         self.myLocation = "\(result.address_name)"
                         UserDefaults.standard.set(self.myLocation, forKey: "myLocation")
                         self.nx = self.locations[result.address_name]!.0
-                        self.nx = self.locations[result.address_name]!.1
+                        self.ny = self.locations[result.address_name]!.1
                         self.longitude = self.locations[result.address_name]!.2
                         self.latitude = self.locations[result.address_name]!.3
+                        self.fetchRecordID(using: self.uuid)
+                        self.updateLocationInDatabase()
                     }
                 } else {
                     DispatchQueue.main.async {
@@ -74,6 +80,47 @@ class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
                 }
             }
         }
+    }
+    
+    func updateLocationInDatabase() {
+        let url = "http://211.188.54.174:1337/alarms/\(id)"
+        let parameters: [String: Any] = [
+            "nx": nx,
+            "ny": ny
+        ]
+        
+        AF.request(url, method: .put, parameters: parameters, encoding: JSONEncoding.default)
+            .validate()
+            .response { response in
+                switch response.result {
+                case .success:
+                    print("위치 정보가 성공적으로 업데이트되었습니다.")
+                case .failure(let error):
+                    print("위치 정보 업데이트 실패: \(error)")
+                    if let data = response.data, let errorMessage = String(data: data, encoding: .utf8) {
+                        print("서버 응답: \(errorMessage)")
+                    }
+                }
+            }
+    }
+    
+    func fetchRecordID(using uuid: String) {
+        let url = "http://211.188.54.174:1337/alarms?uuid=\(uuid)"
+        
+        AF.request(url)
+               .validate()
+               .responseDecodable(of: [Record].self) { response in
+                   switch response.result {
+                   case .success(let records):
+                       if let firstRecord = records.first {
+                           self.id = firstRecord.id
+                           print(firstRecord.id)
+                       } else {
+                       }
+                   case .failure(let error):
+                       print("Error: \(error)")
+                   }
+               }
     }
 }
 
