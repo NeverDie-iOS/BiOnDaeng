178bb2f251f0aa3209e062793b14a4ed4da1f891
hyunsoo <mm083434@gmail.com>
2024-10-17 06:03:25 +0900

    .

---
+++ BiOnDaengApp.swift
@@ -0,0 +1,74 @@
+import SwiftUI
+import Firebase
+import UserNotifications
+import KakaoSDKCommon
+
+@main
+struct BiOnDaengApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @AppStorage("uuid") private var uuid: String = ""
+    
+    init() {
+        KakaoSDK.initSDK(appKey: "d5db3d55c891ebc5cf8b961bb5ca0131")
+        if uuid.isEmpty {
+            uuid = UUID().uuidString
+        }
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            SplashScreen()
+        }
+    }
+    
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
+    @AppStorage("notificationPermission") var notificationPermission: Bool = false
+    @AppStorage("bionNotifi") var bionNotifi: Bool = false
+    @AppStorage("fcmToken") private var fcmToken: String = ""
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        requestNotificationAuthorization()
+        
+        Messaging.messaging().delegate = self
+        return true
+    }
+    
+    func requestNotificationAuthorization() {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if let error = error {
+                print("알림 권한 요청 실패: \(error)")
+                return
+            }
+            self.notificationPermission = granted
+            self.bionNotifi = granted
+        }
+        
+        center.delegate = self
+        UIApplication.shared.registerForRemoteNotifications()
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("등록 토큰 수신: \(String(describing: fcmToken))")
+        
+        guard let fcmToken = fcmToken else { return }
+            self.fcmToken = fcmToken
+    }
+}
+
+extension Bundle {
+    var gisangAuth: String {
+        guard let file = self.path(forResource: "MyPrivacyInfo", ofType: "plist") else { fatalError("MyPrivacyInfo.plist 파일이 없습니다.") }
+        guard let resource = NSDictionary(contentsOfFile: file) else { fatalError("파일 형식 에러") }
+        guard let key = resource["gisangAuth"] as? String else { fatalError("키값 에러")}
+        return key
+    }
+}
---
+++ View/ShareView.swift
@@ -0,0 +1,286 @@
+import SwiftUI
+import SafariServices
+import KakaoSDKCommon
+import KakaoSDKShare
+
+struct ShareView: View {
+    @Binding var showShareView: Bool
+    var myLocation: String
+    var rainfall: String
+    var precipitationType: String
+    
+    var body: some View {
+        ZStack {
+            Color.black.opacity(0.7)
+                .edgesIgnoringSafeArea(.all)
+                .onTapGesture {
+                    showShareView = false
+                }
+            
+            VStack {
+                ShareImageView(myLocation: myLocation, rainfall: rainfall, precipitationType: precipitationType)
+                
+                HStack {
+                    Button(action: {
+                        shareToKakao()
+                    }) {
+                        Image("KakaoShareButton")
+                            .resizable()
+                            .frame(width: 56, height: 56)
+                    }
+                    Spacer()
+                    Button(action: {
+                        shareToInstagram()
+                    }) {
+                        Image("InstaShareButton")
+                            .resizable()
+                            .frame(width: 56, height: 56)
+                    }
+                    Spacer()
+                    Button(action: {
+                        shareContent()
+                    }) {
+                        Image("ShareButton")
+                            .resizable()
+                            .frame(width: 56, height: 56)
+                    }
+                }
+                .padding(.top, 45)
+                .padding(.bottom, 45)
+                .padding(.horizontal, 30)
+            }
+            .frame(width: UIScreen.main.bounds.width * 0.92, height: UIScreen.main.bounds.height * 0.6)
+            .background(Color.white)
+            .cornerRadius(13)
+            .offset(y: 60)
+        }
+    }
+    
+    func shareContent() {
+        let imageToShare = ShareImageView(myLocation: myLocation, rainfall: rainfall, precipitationType: precipitationType).getImage() ?? UIImage()
+        
+        let activityViewController = UIActivityViewController(activityItems: [imageToShare], applicationActivities: nil)
+        
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let rootViewController = windowScene.windows.first?.rootViewController {
+            rootViewController.present(activityViewController, animated: true, completion: nil)
+        }
+    }
+    
+    func shareToKakao() {
+        guard let imageToShare = ShareImageView(myLocation: myLocation, rainfall: rainfall, precipitationType: precipitationType).getImage() else {
+            print("Failed to get image.")
+            return
+        }
+        
+        ShareApi.shared.imageUpload(image: imageToShare) { (imageUploadResult, error) in
+            if let error = error {
+                print("Error uploading image: \(error)")
+                return
+            }
+            
+            guard let uploadedImageURL = imageUploadResult?.infos.original.url else {
+                print("Failed to get uploaded image URL.")
+                return
+            }
+            
+            let feedTemplateJsonStringData =
+            """
+            {
+                "object_type": "feed",
+                "content": {
+                    "title": "오늘의 날씨는~?",
+                    "description": "#외출 전 비가 오면 알려주는 댕댕이",
+                    "image_url": "\(uploadedImageURL)",
+                    "link": {
+                        "mobile_web_url": "https://apps.apple.com/us/app/%EB%B9%84%EC%98%A8%EB%8C%95/id6736566563",
+                        "web_url": "https://apps.apple.com/us/app/%EB%B9%84%EC%98%A8%EB%8C%95/id6736566563"
+                    }
+                },
+                "buttons": [
+                    {
+                        "title": "앱으로 보기",
+                        "link": {
+                            "ios_execution_params": "key1=value1&key2=value2"
+                        }
+                    }
+                ]
+            }
+            """.data(using: .utf8)!
+            
+            guard let templatable = try? JSONSerialization.jsonObject(with: feedTemplateJsonStringData, options: []) as? [String: Any] else {
+                print("Failed to decode feed template")
+                return
+            }
+
+            if ShareApi.isKakaoTalkSharingAvailable() {
+                ShareApi.shared.shareDefault(templateObject: templatable) { sharingResult, error in
+                    if let error = error {
+                        print("Error: \(error)")
+                    } else {
+                        print("shareDefault() success.")
+                        if let sharingResult = sharingResult {
+                            UIApplication.shared.open(sharingResult.url, options: [:], completionHandler: nil)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func shareToInstagram() {
+        func isInstagramInstalled() -> Bool {
+            guard let instagramURL = URL(string: "instagram-stories://share") else {
+                return false
+            }
+            return UIApplication.shared.canOpenURL(instagramURL)
+        }
+        
+        guard let imageToShare = ShareImageView(myLocation: myLocation, rainfall: rainfall, precipitationType: precipitationType).getImage() else {
+            print("Failed to get image.")
+            return
+        }
+
+        // 인스타그램에서 요구하는 이미지 데이터로 변환
+        guard let imageData = imageToShare.pngData() else {
+            print("Failed to convert image to PNG data.")
+            return
+        }
+
+        // 임시 파일로 저장
+        let temporaryDirectory = FileManager.default.temporaryDirectory
+        let instagramImageURL = temporaryDirectory.appendingPathComponent("instagram.png")
+        
+        do {
+            try imageData.write(to: instagramImageURL)
+        } catch {
+            print("Error writing image to temporary directory: \(error)")
+            return
+        }
+
+        // UIActivityViewController를 사용하여 인스타그램에 공유
+        let activityViewController = UIActivityViewController(activityItems: [instagramImageURL], applicationActivities: nil)
+        activityViewController.excludedActivityTypes = [.postToFacebook, .postToTwitter] // 필요시 제외할 액티비티 타입 설정
+
+        // 현재의 ViewController에서 Activity View Controller 표시
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let rootViewController = windowScene.windows.first?.rootViewController {
+            rootViewController.present(activityViewController, animated: true, completion: nil)
+        }
+    }
+
+}
+
+struct ShareImageView: View {
+    @StateObject private var weatherShort = ShortTermModel()
+    var myLocation: String
+    var rainfall: String
+    var precipitationType: String
+    
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            let mainImage = (precipitationType.isEmpty || precipitationType == "0" || precipitationType == "3" || precipitationType == "7") ? "ShareBackground_Sunny" : "ShareBackground"
+            let message = mainImage == "ShareBackground_Sunny" ?
+            "맑댕" : "\(rainfall) 비온댕"
+            
+            Image(mainImage)
+                .resizable()
+                .scaledToFit()
+            
+            VStack(alignment: .leading, spacing: 0) {
+                Text(currentTime())
+                    .font(.pretendardExtraLight(size: 12))
+                    .foregroundColor(.white)
+                    .padding(.leading, 19)
+                    .opacity(0.5)
+                
+                Text("\(myLocation)은")
+                    .font(.pretendardExtraLight(size: 15))
+                    .foregroundColor(.white)
+                    .padding(.leading, 19)
+                    .opacity(0.5)
+                
+                Text(message)
+                    .font(.pretendardLight(size: 28))
+                    .foregroundColor(.white)
+                    .padding(.leading, 19)
+            }
+            .padding(.top, 10)
+        }
+        .padding(.horizontal, 19)
+        .padding(.top, 20)
+    }
+    
+    func currentTime() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "a hh:mm"
+        formatter.locale = Locale(identifier: "ko_KR")
+        let dateString = formatter.string(from: Date())
+        return dateString.replacingOccurrences(of: "AM", with: "오전").replacingOccurrences(of: "PM", with: "오후")
+    }
+    
+    func getImage() -> UIImage? {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 300, height: 300))
+        return renderer.image { context in
+            
+            context.cgContext.setFillColor(UIColor.black.cgColor)
+            context.cgContext.fill(CGRect(x: 0, y: 0, width: 300, height: 300))
+            
+            let mainImage = (precipitationType.isEmpty || precipitationType == "0" || precipitationType == "3" || precipitationType == "7") ? "ShareBackground_Sunny" : "ShareBackground"
+            
+            if let background = UIImage(named: mainImage)?.cgImage {
+                context.cgContext.saveGState()
+                context.cgContext.translateBy(x: 0, y: 300)
+                context.cgContext.scaleBy(x: 1, y: -1)
+                context.cgContext.draw(background, in: CGRect(x: 0, y: 0, width: 300, height: 300))
+                context.cgContext.restoreGState()
+            }
+            
+            let timeAttributes: [NSAttributedString.Key: Any] = [
+                .font: UIFont(name: "Pretendard-ExtraLight", size: 12) ?? UIFont.systemFont(ofSize: 12),
+                .foregroundColor: UIColor.white.withAlphaComponent(0.5)
+            ]
+            
+            let locationAttributes: [NSAttributedString.Key: Any] = [
+                .font: UIFont(name: "Pretendard-ExtraLight", size: 15) ?? UIFont.systemFont(ofSize: 15),
+                .foregroundColor: UIColor.white.withAlphaComponent(0.5)
+            ]
+            
+            
+            let messageAttributes: [NSAttributedString.Key: Any] = [
+                .font: UIFont(name: "Pretendard-Light", size: 28) ?? UIFont.systemFont(ofSize: 28),
+                .foregroundColor: UIColor.white
+            ]
+            
+            let timeText = currentTime()
+            let locationText = "\(myLocation)"
+            let message = mainImage == "ShareBackground_Sunny" ? "맑댕" : "\(rainfall) 비온댕"
+            
+            timeText.draw(at: CGPoint(x: 19, y: 10), withAttributes: timeAttributes)
+            locationText.draw(at: CGPoint(x: 19, y: 23), withAttributes: locationAttributes)
+            message.draw(at: CGPoint(x: 19, y: 37), withAttributes: messageAttributes)
+        }
+    }
+}
+
+struct SafariView: UIViewControllerRepresentable {
+    let url: URL
+    func makeUIViewController(context: Context) -> SFSafariViewController {
+        return SFSafariViewController(url: url)
+    }
+    func updateUIViewController(_ uiViewController: SFSafariViewController,
+        context: Context) {}
+}
+
+struct Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            MainView()
+                .previewDevice(PreviewDevice(rawValue: "iPhone 15 Pro"))
+                .previewDisplayName("iPhone 15 Pro")
+            MainView()
+                .previewDevice(PreviewDevice(rawValue: "iPhone SE (3rd generation)"))
+                .previewDisplayName("iPhone SE (3rd generation)")
+        }
+    }
+}
