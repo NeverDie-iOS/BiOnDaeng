f1cd652b8dc356b142479c6eab9a4e64d5cb6f34
hyunsoo <mm083434@gmail.com>
2024-10-17 19:25:19 +0900

    ⚡️ : Share기능 전체 완성

--- BiOnDaeng/View/ShareView.swift
+++ BiOnDaeng/View/ShareView.swift
@@ -89,7 +89,7 @@ struct ShareView: View {
             {
                 "object_type": "feed",
                 "content": {
-                    "title": "오늘의 날씨는~?",
+                    "title": "지금 여기 날씨는~?",
                     "description": "#외출 전 비가 오면 알려주는 댕댕이",
                     "image_url": "\(uploadedImageURL)",
                     "link": {
@@ -112,7 +112,7 @@ struct ShareView: View {
                 print("Failed to decode feed template")
                 return
             }
-
+            
             if ShareApi.isKakaoTalkSharingAvailable() {
                 ShareApi.shared.shareDefault(templateObject: templatable) { sharingResult, error in
                     if let error = error {
@@ -129,158 +129,151 @@ struct ShareView: View {
     }
     
     func shareToInstagram() {
-        func isInstagramInstalled() -> Bool {
-            guard let instagramURL = URL(string: "instagram-stories://share") else {
-                return false
-            }
-            return UIApplication.shared.canOpenURL(instagramURL)
-        }
         
         guard let imageToShare = ShareImageView(myLocation: myLocation, rainfall: rainfall, precipitationType: precipitationType).getImage() else {
-            print("Failed to get image.")
             return
         }
 
-        // 인스타그램에서 요구하는 이미지 데이터로 변환
-        guard let imageData = imageToShare.pngData() else {
-            print("Failed to convert image to PNG data.")
-            return
-        }
-
-        // 임시 파일로 저장
         let temporaryDirectory = FileManager.default.temporaryDirectory
-        let instagramImageURL = temporaryDirectory.appendingPathComponent("instagram.png")
+        let instagramImageURL = temporaryDirectory.appendingPathComponent("instagram.igo")
         
-        do {
-            try imageData.write(to: instagramImageURL)
-        } catch {
-            print("Error writing image to temporary directory: \(error)")
-            return
-        }
-
-        // UIActivityViewController를 사용하여 인스타그램에 공유
-        let activityViewController = UIActivityViewController(activityItems: [instagramImageURL], applicationActivities: nil)
-        activityViewController.excludedActivityTypes = [.postToFacebook, .postToTwitter] // 필요시 제외할 액티비티 타입 설정
-
-        // 현재의 ViewController에서 Activity View Controller 표시
-        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-           let rootViewController = windowScene.windows.first?.rootViewController {
-            rootViewController.present(activityViewController, animated: true, completion: nil)
-        }
-    }
-
-}
-
-struct ShareImageView: View {
-    @StateObject private var weatherShort = ShortTermModel()
-    var myLocation: String
-    var rainfall: String
-    var precipitationType: String
-    
-    var body: some View {
-        ZStack(alignment: .topLeading) {
-            let mainImage = (precipitationType.isEmpty || precipitationType == "0" || precipitationType == "3" || precipitationType == "7") ? "ShareBackground_Sunny" : "ShareBackground"
-            let message = mainImage == "ShareBackground_Sunny" ?
-            "맑댕" : "\(rainfall) 비온댕"
-            
-            Image(mainImage)
-                .resizable()
-                .scaledToFit()
-            
-            VStack(alignment: .leading, spacing: 0) {
-                Text(currentTime())
-                    .font(.pretendardExtraLight(size: 12))
-                    .foregroundColor(.white)
-                    .padding(.leading, 19)
-                    .opacity(0.5)
+        if let imageData = imageToShare.pngData() {
+            do {
+                try imageData.write(to: instagramImageURL)
+  
+                let shareURL = URL(string: Bundle.main.metaAppKey)!
                 
-                Text("\(myLocation)은")
-                    .font(.pretendardExtraLight(size: 15))
-                    .foregroundColor(.white)
-                    .padding(.leading, 19)
-                    .opacity(0.5)
+                let pasteboardItems: [String: Any] = [
+                    "com.instagram.sharedSticker.stickerImage": imageData,
+                    "com.instagram.sharedSticker.backgroundTopColor" : "#FFFFFF",
+                    "com.instagram.sharedSticker.backgroundBottomColor" : "#FFFFFF"
+                ]
                 
-                Text(message)
-                    .font(.pretendardLight(size: 28))
-                    .foregroundColor(.white)
-                    .padding(.leading, 19)
+                let pasteboardOptions = [
+                    UIPasteboard.OptionsKey.expirationDate : Date().addingTimeInterval(300)
+                ]
+                UIPasteboard.general.setItems([pasteboardItems], options: pasteboardOptions)
+        
+                if UIApplication.shared.canOpenURL(shareURL) {
+                    UIApplication.shared.open(shareURL, options: [:], completionHandler: nil)
+                }
+            } catch {
+                print("Error writing image data \(error)")
             }
-            .padding(.top, 10)
         }
-        .padding(.horizontal, 19)
-        .padding(.top, 20)
+    }
+}
+    
+    struct ShareImageView: View {
+        @StateObject private var weatherShort = ShortTermModel()
+        var myLocation: String
+        var rainfall: String
+        var precipitationType: String
+        
+        var body: some View {
+            ZStack(alignment: .topLeading) {
+                let mainImage = (precipitationType.isEmpty || precipitationType == "0" || precipitationType == "3" || precipitationType == "7") ? "ShareBackground_Sunny" : "ShareBackground"
+                let message = mainImage == "ShareBackground_Sunny" ?
+                "맑댕" : "\(rainfall) 비온댕"
+                
+                Image(mainImage)
+                    .resizable()
+                    .scaledToFit()
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    Text(currentTime())
+                        .font(.pretendardExtraLight(size: 12))
+                        .foregroundColor(.white)
+                        .padding(.leading, 19)
+                        .opacity(0.5)
+                    
+                    Text("\(myLocation)은")
+                        .font(.pretendardExtraLight(size: 15))
+                        .foregroundColor(.white)
+                        .padding(.leading, 19)
+                        .opacity(0.5)
+                    
+                    Text(message)
+                        .font(.pretendardLight(size: 28))
+                        .foregroundColor(.white)
+                        .padding(.leading, 19)
+                }
+                .padding(.top, 10)
+            }
+            .padding(.horizontal, 19)
+            .padding(.top, 20)
+        }
+        
+        func currentTime() -> String {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "a hh:mm"
+            formatter.locale = Locale(identifier: "ko_KR")
+            let dateString = formatter.string(from: Date())
+            return dateString.replacingOccurrences(of: "AM", with: "오전").replacingOccurrences(of: "PM", with: "오후")
+        }
+        
+        func getImage() -> UIImage? {
+            let renderer = UIGraphicsImageRenderer(size: CGSize(width: 300, height: 300))
+            return renderer.image { context in
+                
+                context.cgContext.setFillColor(UIColor(red: 0x00/255.0, green: 0x6F/255.0, blue: 0xC2/255.0, alpha: 1.0).cgColor)
+                context.cgContext.fill(CGRect(x: 0, y: 0, width: 300, height: 300))
+                
+                let mainImage = (precipitationType.isEmpty || precipitationType == "0" || precipitationType == "3" || precipitationType == "7") ? "ShareBackground_Sunny" : "ShareBackground"
+                
+                if let background = UIImage(named: mainImage)?.cgImage {
+                    context.cgContext.saveGState()
+                    context.cgContext.translateBy(x: 0, y: 300)
+                    context.cgContext.scaleBy(x: 1, y: -1)
+                    context.cgContext.draw(background, in: CGRect(x: 0, y: 0, width: 300, height: 300))
+                    context.cgContext.restoreGState()
+                }
+                
+                let timeAttributes: [NSAttributedString.Key: Any] = [
+                    .font: UIFont(name: "Pretendard-ExtraLight", size: 12) ?? UIFont.systemFont(ofSize: 12),
+                    .foregroundColor: UIColor.white.withAlphaComponent(0.5)
+                ]
+                
+                let locationAttributes: [NSAttributedString.Key: Any] = [
+                    .font: UIFont(name: "Pretendard-ExtraLight", size: 15) ?? UIFont.systemFont(ofSize: 15),
+                    .foregroundColor: UIColor.white.withAlphaComponent(0.5)
+                ]
+                
+                
+                let messageAttributes: [NSAttributedString.Key: Any] = [
+                    .font: UIFont(name: "Pretendard-Light", size: 28) ?? UIFont.systemFont(ofSize: 28),
+                    .foregroundColor: UIColor.white
+                ]
+                
+                let timeText = currentTime()
+                let locationText = "\(myLocation)"
+                let message = mainImage == "ShareBackground_Sunny" ? "맑댕" : "\(rainfall) 비온댕"
+                
+                timeText.draw(at: CGPoint(x: 19, y: 10), withAttributes: timeAttributes)
+                locationText.draw(at: CGPoint(x: 19, y: 23), withAttributes: locationAttributes)
+                message.draw(at: CGPoint(x: 19, y: 37), withAttributes: messageAttributes)
+            }
+        }
     }
     
-    func currentTime() -> String {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "a hh:mm"
-        formatter.locale = Locale(identifier: "ko_KR")
-        let dateString = formatter.string(from: Date())
-        return dateString.replacingOccurrences(of: "AM", with: "오전").replacingOccurrences(of: "PM", with: "오후")
+    struct SafariView: UIViewControllerRepresentable {
+        let url: URL
+        func makeUIViewController(context: Context) -> SFSafariViewController {
+            return SFSafariViewController(url: url)
+        }
+        func updateUIViewController(_ uiViewController: SFSafariViewController,
+                                    context: Context) {}
     }
     
-    func getImage() -> UIImage? {
-        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 300, height: 300))
-        return renderer.image { context in
-            
-            context.cgContext.setFillColor(UIColor.black.cgColor)
-            context.cgContext.fill(CGRect(x: 0, y: 0, width: 300, height: 300))
-            
-            let mainImage = (precipitationType.isEmpty || precipitationType == "0" || precipitationType == "3" || precipitationType == "7") ? "ShareBackground_Sunny" : "ShareBackground"
-            
-            if let background = UIImage(named: mainImage)?.cgImage {
-                context.cgContext.saveGState()
-                context.cgContext.translateBy(x: 0, y: 300)
-                context.cgContext.scaleBy(x: 1, y: -1)
-                context.cgContext.draw(background, in: CGRect(x: 0, y: 0, width: 300, height: 300))
-                context.cgContext.restoreGState()
+    struct Previews: PreviewProvider {
+        static var previews: some View {
+            Group {
+                MainView()
+                    .previewDevice(PreviewDevice(rawValue: "iPhone 15 Pro"))
+                    .previewDisplayName("iPhone 15 Pro")
+                MainView()
+                    .previewDevice(PreviewDevice(rawValue: "iPhone SE (3rd generation)"))
+                    .previewDisplayName("iPhone SE (3rd generation)")
             }
-            
-            let timeAttributes: [NSAttributedString.Key: Any] = [
-                .font: UIFont(name: "Pretendard-ExtraLight", size: 12) ?? UIFont.systemFont(ofSize: 12),
-                .foregroundColor: UIColor.white.withAlphaComponent(0.5)
-            ]
-            
-            let locationAttributes: [NSAttributedString.Key: Any] = [
-                .font: UIFont(name: "Pretendard-ExtraLight", size: 15) ?? UIFont.systemFont(ofSize: 15),
-                .foregroundColor: UIColor.white.withAlphaComponent(0.5)
-            ]
-            
-            
-            let messageAttributes: [NSAttributedString.Key: Any] = [
-                .font: UIFont(name: "Pretendard-Light", size: 28) ?? UIFont.systemFont(ofSize: 28),
-                .foregroundColor: UIColor.white
-            ]
-            
-            let timeText = currentTime()
-            let locationText = "\(myLocation)"
-            let message = mainImage == "ShareBackground_Sunny" ? "맑댕" : "\(rainfall) 비온댕"
-            
-            timeText.draw(at: CGPoint(x: 19, y: 10), withAttributes: timeAttributes)
-            locationText.draw(at: CGPoint(x: 19, y: 23), withAttributes: locationAttributes)
-            message.draw(at: CGPoint(x: 19, y: 37), withAttributes: messageAttributes)
         }
     }
-}
-
-struct SafariView: UIViewControllerRepresentable {
-    let url: URL
-    func makeUIViewController(context: Context) -> SFSafariViewController {
-        return SFSafariViewController(url: url)
-    }
-    func updateUIViewController(_ uiViewController: SFSafariViewController,
-        context: Context) {}
-}
-
-struct Previews: PreviewProvider {
-    static var previews: some View {
-        Group {
-            MainView()
-                .previewDevice(PreviewDevice(rawValue: "iPhone 15 Pro"))
-                .previewDisplayName("iPhone 15 Pro")
-            MainView()
-                .previewDevice(PreviewDevice(rawValue: "iPhone SE (3rd generation)"))
-                .previewDisplayName("iPhone SE (3rd generation)")
-        }
-    }
-}
